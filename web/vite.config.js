eff650f57e81cfc61b401c9a469e2f4111c24007
zhaocongqi <1229896069@qq.com>
2026-06-01 08:37:19 +0000

    chore(web): 开发服务器监听 0.0.0.0 支持外部访问

--- web/vite.config.js
+++ web/vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: '0.0.0.0',
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8080',
